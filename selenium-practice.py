77b30e30afec9b767f582cc1e26b9297d1e9c0c7
Alexander Parkhill <alexanderparkhill@gmail.com>
2024-07-06 14:42:10 +1000

    Selenium cleaned up

--- selenium-practice.py
+++ selenium-practice.py
@@ -3,19 +3,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
-import random
-
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import pyautogui
-
-
-def typer_call():
-    time.sleep(2)
-    pyautogui.write("Slave Degradation", 0.04)
-    return " "
+import time
+import random
 
 driver_path = '/Users/alexanderparkhill/chromedriver'
 
@@ -23,6 +15,7 @@ driver_path = '/Users/alexanderparkhill/chromedriver'
 service = Service(driver_path)
 
 chrome_options = webdriver.ChromeOptions()
+
 prefs = {"profile.default_content_setting_values.notifications" : 2}
 chrome_options.add_experimental_option("prefs",prefs)
 chrome_options.add_argument('--disable-infobars')
@@ -35,48 +28,7 @@ chrome_options.add_argument('--disable-features=EnableEphemeralFlashPermission')
 # Initialize the ChromeDriver with the options
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Open a webpage
-driver.get('https://www.typeforme.net/task/43c2042309fb4eb8bb687a5744c198f1')
-
-element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, 'v-btn--elevated')))
-
-# Find the search box element
-#search_box = driver.find_element(By.ID, 'input-119')
-button = driver.find_element(By.CLASS_NAME, 'v-btn--elevated')
-
-# Click the button
-button.click()
-time.sleep(3)
-
-# Enter text into the search box
-
-#search_box = driver.find_element(By.CLASS_NAME, "v-field__input")
-
-#search_box.click()
-#time.sleep(1)
-#search_box.send_keys(typer_call())
-#time.sleep(1)
-#search_box.send_keys(Keys.RETURN)
-#time.sleep(3)
-
-wait = WebDriverWait(driver, 10)
-element2 = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@href="/task/43c2042309fb4eb8bb687a5744c198f1"]')))
-
-# Click the element
-element2.click()
-
-
-# Find and click the task button
-
-#wait = WebDriverWait(driver, 10)
-#element3 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="c833b5acc9aa45eaa79d0d18692cfb0a"]/div[1]/div/div[4]/div/div[2]/button')))
-
-#element3.click()
-
-
-#Interruption checks
-
+#---------------------------Functions---------------------------------#
 
 def interruption_loop():
     print("Entering Interruption Loop")
@@ -93,7 +45,6 @@ def mistake_loop():
     print("Exiting Mistake Loop")
     time.sleep(1)
       
-
 def check_for_interruption_screen():
     try: 
         interruption_overlay = driver.find_element(By.ID, 'interruptionOverlay')
@@ -101,7 +52,6 @@ def check_for_interruption_screen():
             return True
     except:
         return False
-
 
 def check_for_mistake():
     try: 
@@ -111,14 +61,39 @@ def check_for_mistake():
     except:
         return False
 
-########## Execution here ###########
+#---------------------------Opening page---------------------------------#
+# Open the webpage
+driver.get('https://www.typeforme.net/task/43c2042309fb4eb8bb687a5744c198f1')
 
+# Wait for the 'accept' button
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'v-btn--elevated')))
+
+# Define the button
+button = driver.find_element(By.CLASS_NAME, 'v-btn--elevated')
+
+# Click the button
+button.click()
+
+# Wait until the task becomes visible
+wait = WebDriverWait(driver, 10)
+task_link = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@href="/task/43c2042309fb4eb8bb687a5744c198f1"]')))
+
+# Click the element
+task_link.click()
+
+# Pause before starting to type
 print("Arrived on page, starting typing cycle in 3 seconds")
-time.sleep(4)
+time.sleep(2)
 
-## Initial mistake trigger
+
+#---------------------------Typing begins---------------------------------#
+
+## Trigger initial mistake to prevent capital letter issue
 
 pyautogui.write("z", 0.1)
+
+## Complete the task
 
 for i in range(25):
     
@@ -144,8 +119,8 @@ for i in range(25):
             pyautogui.write("I mustr", 0.05)
 
 
+# Wait to observe results before browser closes
 
-# Wait for a few seconds to see the results
-time.sleep(3)
+time.sleep(30)
 
 
